d6a2a57a5008a7330e26457cb41082965dfdd720
Pietro Zanotim <pietroalveszanotim@gmail.com>
2025-12-25 16:33:29 -0300

    Inclusão de validações na função listarSessoes

--- sessoes.c
+++ sessoes.c
@@ -25,7 +25,9 @@ void listarSessoes(Sessoes *sessao, int quantidadeSessoes, Usuarios *usuario, in
         printf("Sala: %d\n", sessao[i].sala);
         printf("Valor: %d\n", sessao[i].valorIngresso);
         printf("Assentos:\n");
+        printf("    (C0) (C1) (C2) (C3) (C4) (C5) (C6) (C7) (C8) (C9) (C10)");
         for(int i=0; i<15; i++){
+            printf("(L%d)", i);
             for(int a=0; a<15; a++){
             if(a==14){
                 printf("[%d]\n",sessao[i].assento[i][a]); // Para pular para outra "linha" de assentos;
@@ -34,8 +36,8 @@ void listarSessoes(Sessoes *sessao, int quantidadeSessoes, Usuarios *usuario, in
             }
             }
         }
-        puts("");
-
+        printf("_________________________________________________________________\n");
+        printf("                             TELA\n");
     }
         char resposta;
 
@@ -61,6 +63,9 @@ void listarSessoes(Sessoes *sessao, int quantidadeSessoes, Usuarios *usuario, in
     if(resposta=='S'){
         int id;
         int indiceSessao;
+        int linhaAssento;
+        int colunaAssento;
+
         printf("\nDigite o id do filme: ");
         
         do{
@@ -69,11 +74,27 @@ void listarSessoes(Sessoes *sessao, int quantidadeSessoes, Usuarios *usuario, in
             indiceSessao = buscaSessao(sessao, id, quantidadeSessoes);
         
             if(indiceSessao==-1){
-                printf("\nId não localizado, digite novamente: ");
+                printf("\nId não localizado, digite novamente.\nDigite o id do filme: ");
+            } else if(usuario[indiceUsuario].idade < sessao[indiceSessao].limIdade){
+                printf("\nIdade não permitida para este filme, tente novamente.\nDigite o id do filme: ");
+                indiceSessao=-1;
+            } else if(usuario[indiceUsuario].saldo < sessao[indiceSessao].valorIngresso){
+                printf("\nNão foi possível adquirir o ingresso, valor insuficiente. Tente novamente.\nDigite o id do filme: ");
+                indiceSessao=-1;
             }
         }while(indiceSessao==-1);
 
-        printf("\nDigite o assento desejado: ");
+        do{
+            printf("\nDigite a linha do assento desejado (Ex.1): ");
+            scanf("%d", &linhaAssento);
+
+            printf("\nDigite a coluna do assento desejado (Ex.1): ");
+            scanf("%d", &colunaAssento);
+
+            if(sessao[indiceSessao].assento[linhaAssento][colunaAssento]=='X'){
+                printf("Sentimos muito, este assento está ocupado...\n");
+            }
+        } while (sessao[indiceSessao].assento[linhaAssento][colunaAssento]=='X');
 
     } else {
         limparTela();
